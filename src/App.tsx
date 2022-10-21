8c7c275260750380809eb94bdb730538d81ee63e
Akira Yamamoto <yama2akira@gmail.com>
2022-10-21 09:29:45 +0900

    classical fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { FC } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ClassicalFetchA } from './components/ClassicalFetchA';
+import { ClassicalFetchB } from './components/ClassicalFetchB';
 import { Layout } from './components/Layout';
+import { StateProvider } from './context/StateProvider';
 
 const App: FC = () => {
   return (
     <BrowserRouter>
-      <Layout>App</Layout>
+      <StateProvider>
+        <Layout>
+          <Routes>
+            <Route path="/fetch-a" element={<ClassicalFetchA />} />
+            <Route path="/fetch-b" element={<ClassicalFetchB />} />
+          </Routes>
+        </Layout>
+      </StateProvider>
     </BrowserRouter>
   );
 };
